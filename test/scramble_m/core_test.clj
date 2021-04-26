b90483af6e56e30bf987dc17dcafe25568b8de45
Ilya G. Mirin <ilya.mirin@gmail.com>
2021-04-27 09:53:37 +1000

    Speed testing added.

--- test/scramble_m/core_test.clj
+++ test/scramble_m/core_test.clj
@@ -2,7 +2,7 @@
   (:require [clojure.test :refer :all]
             [scramble-m.core :refer :all]))
 
-(deftest scramble-function-test
+(deftest scramble-function-basic-test
   (testing "Some of the simplest scrambling examples don't work."
     (is (true? (scramble "algorithm" "logarithm")))
     (is (true? (scramble "algorithm" "loga")))
@@ -13,3 +13,9 @@
     (is (false? (scramble "algorithme" "logarithmee")))
     (is (false? (scramble "test" "logariphmee")))))
 
+;;TODO: add errors and anything tests
+
+(deftest scramble-function-speed-test
+  (testing "One iteration must spend less time then 1 millisecond."
+    (def spd (re-find #"[0-9]+\.[0-9]+" (with-out-str (time (scramble "algorithm" "logarithm")))))
+  (is (<= (Double. spd) 0.1))))
